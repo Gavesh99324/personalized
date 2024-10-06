f7c52e0e2f3d97aae09988cad1a82e145c922c30
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-10-06 11:01:02 +0530

    update whole frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./Components/Navbar/Navbar";
-import { FooterContainer } from "./containers/footer";
 
 function App() {
 
-  const current_theme = localStorage.getItem('current_theme')
-  const [theme, setTheme] = useState(current_theme ? current_theme : 'light')
-
-  useEffect(() => {
-    localStorage.setItem('current_theme', theme)
-  }, [theme])
-
-
   return (
-    <div className={`container ${theme}`}>
-      <Navbar theme={theme} setTheme={setTheme}/>
-      <FooterContainer />
+    <div>
+    
     </div>
   );
 }
 
 export default App;
+
+
 
--- src/Components/icons/iconsIndex.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Icon } from '.iconStyles/icons'
-
-export default function Icons ({ className}) {
-    return <Icon className={className} />
-}
