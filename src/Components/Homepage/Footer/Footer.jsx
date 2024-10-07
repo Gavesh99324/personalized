ea2ccbf77f4972a28eeb82ac19618167364ef80c
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-10-08 02:09:13 +0530

    updated

--- src/Components/Homepage/Footer/Footer.jsx
+++ src/Components/Homepage/Footer/Footer.jsx
@@ -1,5 +1,55 @@
 import React from 'react'
 import './Footer.css'
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaTwitch } from 'react-icons/fa'
+
+
+const sections = [
+  {
+    title: 'Who We Are',
+    items: ['About the Us', 'Contact Us']
+  },
+  {
+    title: 'Privacy & Terms',
+    items: ['Privacy Policy', 'Terms and Conditions']
+  },
+  {
+    title: 'Helpful Resources',
+    items: ['Mental Health Resources', 'Support/Help']
+  },
+  {
+    title: 'Community and Support',
+    items: ['Social Media Links', 'Feedback', 'Newsletter Subscription']
+  },
+]
+
+const items = [
+  {
+    name: 'Facebook',
+    icon: FaFacebook,
+    link: 'https://www.facebook.com',
+  },
+  {
+    name: 'Instagram',
+    icon: FaInstagram,
+    link: 'https://www.instagram.com',
+  },
+  {
+    name: 'Twitter',
+    icon: FaTwitter,
+    link: 'https://www.twitter.com',
+  },
+  {
+    name: 'Twitch',
+    icon: FaTwitch,
+    link: 'https://www.twitch.com',
+  },
+  {
+    name: 'Github',
+    icon: FaGithub,
+    link: 'https://www.github.com',
+  },
+]
+
 
 export default function Footer() {
   return (
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Homepage/Navbar/Navbar'
+import Footer from '../Components/Homepage/Footer/Footer'
 
 export default function HomePage() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   )
 }
