2f466c933e5c6670f97fef7900175f831947f961
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-10-30 16:19:45 +0530

    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import Header from '../Components/Homepage/Header/Header'
 
 export default function HomePage() {
   return (
     <>
     <div>
-      
+      <Header />
     </div>
     </>
   )
 }
+
 
 
 
