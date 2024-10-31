a8d26c0f44299a082154ef513a4d56d680dcbd14
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-10-31 15:41:28 +0530

    update

--- src/Components/Homepage/Header/Header.jsx
+++ src/Components/Homepage/Header/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 import F_Doctor from '../../../Assests/F_Doctor.png';
-import './Header.css'
+import './Header.css';
 
 export default function Header() {
   return (
-    <div className='Doctor'>
-        <img src={F_Doctor} alt="Header" />
+    <div className="header-container">
+      <img className="Doctor-image" src={F_Doctor} alt="Header" />
+      <div className="mission-statement">
+        <h2>Mission</h2>
+        <p>
+          "To provide comprehensive and evidence-based Mental Health Services appropriate to the local context through state-of-the-art approaches to patient care, capacity building, advocacy, community engagement, multi-sector collaboration, and research delivered by competent and reliable staff."
+        </p>
+      </div>
     </div>
-  )
+  );
 }
-
-
-
-
