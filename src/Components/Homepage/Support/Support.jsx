bd0df58d65636c8f9ac6db41e3cdd50061f702d4
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-11-18 22:29:35 +0530

    update

---
+++ src/Components/Homepage/Support/Support.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Support.css';
+
+export default function Support() {
+  return (
+    <div className="support-card-container">
+      <div className="support-card card-1">
+        <h3>Card 1</h3>
+      </div>
+      <div className="support-card card-2">
+        <h3>Card 2</h3>
+      </div>
+      <div className="support-card card-3">
+        <h3>Card 3</h3>
+      </div>
+      <div className="support-card card-4">
+        <h3>Card 4</h3>
+      </div>
+    </div>
+  );
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Header from '../Components/Homepage/Header/Header'
 import Profiles from '../Components/Profiles/Profiles'
 import CardGrid from '../Components/ProfileCards/CardGrid'
+import Support from '../Components/Homepage/Support/Support'
 
 export default function HomePage() {
   return (
     <>
     <div>
       <Header />
+      <Support />
       <Profiles />
       <CardGrid />
     </div>
