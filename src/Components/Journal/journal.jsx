a18f91f1cc60288428a7b80bfed56119e04a79dd
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-01-12 21:18:47 +0530

    update

--- src/Components/Journal/journal.jsx
+++ src/Components/Journal/journal.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Journal.css'
+import Appointment from '../../Assests/Appointment.jpg'
 
 export default function Journal() {
   return (
-    <div>
-      Journal
+    <div className='header-image'>
+      <img className='journal-image' src={Appointment} alt="Appointment" />
     </div>
   )
 }
+
+
+
+
+
+
