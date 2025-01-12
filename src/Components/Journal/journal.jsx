61973e930b8e160277e37f140f4d103b4eea6477
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-01-12 21:46:40 +0530

    update

--- src/Components/Journal/journal.jsx
+++ src/Components/Journal/journal.jsx
@@ -6,6 +6,8 @@ export default function Journal() {
   return (
     <div className='header-image'>
       <img className='journal-image' src={Appointment} alt="Appointment" />
+      <h2 className='journal-header'>Appointment</h2>
+      <h6 className='journal-subheader'>Secure Your Spot for Healing and Support</h6>
     </div>
   )
 }
