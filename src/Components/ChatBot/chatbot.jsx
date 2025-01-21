b1d7d8910f62dfb7d5f57a9c993df2d96f6640fb
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-01-21 20:43:15 +0530

    update

--- src/Components/ChatBot/chatbot.jsx
+++ src/Components/ChatBot/chatbot.jsx
@@ -12,9 +12,32 @@ export default function Chatbot() {
         <h4 className='contact-subtitle'>Connect with Us, Your Support Awaits</h4>
       </div>
     </div>
+      
+    <div className='details-container'>
+      <h3 className='details-header'>Seamless Communication, <br />Global Impact.</h3>
+
+      <div className='card'>
+        <h5 className='address-header'>Head Office</h5>
+        <p className='address-text'>P.O. Box 854, Sir Marcus Fernando Mw.,<br /> Colombo 07</p>
+      </div>
+      <div className='card'>
+        <h5 className='email-header'>Email Us</h5>
+        <p className='email-text'>support@yourdomain.tld <br />hello@yourdomain.tld</p>
+      </div>
+      <div className='card'>
+        <h5 className='telephone-header'>Let's Talk</h5>
+        <p className='telephone-text'>phone: +94 11 222 3333 <br />Fax: +94 11 222 4444</p>
+      </div>
+
+    </div>
+
     </>
   )
 }
+
+
+
+
 
 
 
